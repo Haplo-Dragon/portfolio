54f275c1272571b50d0ed7c9cebbb0f24cafab1d
Ethan Fulbright <haplodragon@gmail.com>
2018-06-15 03:32:48 -0500

    Platform independence (#2)
    * Add file name constants, remove spaces from file names
    
    * Use UTF-8 encoding explicitly for CSV files

--- lament_mod/character.py
+++ lament_mod/character.py
@@ -13,7 +13,7 @@ class LotFPCharacter(object):
 
         self.name = ' - '.join([str(self.counter + 1), self.pcClass])
         self.fdf_name = self.name + '.fdf'
-        self.filled_name = self.name + ' Filled.pdf'
+        self.filled_name = self.name + '_Filled.pdf'
 
         self.alignment = self.align(self.pcClass)
         self.attributes = {item[0]: item[1] for item in self.details['attributes']}
