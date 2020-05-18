6a5302f9303d84e3f9a222b2c8a7a43cf2080140
Ethan Fulbright <haplodragon@gmail.com>
2020-05-18 07:07:59 -0500

    Update lament tests to remove subdomain

--- tests/test_lament.py
+++ tests/test_lament.py
@@ -1,12 +1,12 @@
 import pytest
 import json
 
-LAMENT_SUBDOMAIN = "http://lament.localhost:42000"
+LAMENT_APP_LOCATION = "http://localhost:42000/character"
 
 
 def test_lament_index(client):
     """Does the main page of Lament render properly?"""
-    response = client.get('/', base_url=LAMENT_SUBDOMAIN)
+    response = client.get('/character', base_url=LAMENT_APP_LOCATION)
     assert response.status_code == 200
     list_of_classes = [
         'Fighter',
@@ -34,8 +34,7 @@ def test_bad_data_entered_in_random_field(client, bad_input, expected_message):
     response = client.post(
         '/lament',
         data={'randos': bad_input, 'desired_level': 1},
-        follow_redirects=True,
-        base_url=LAMENT_SUBDOMAIN)
+        follow_redirects=True,)
     assert response.status_code == 200
     assert expected_message in response.data
 
@@ -48,8 +47,7 @@ def test_generating_over_20_characters(client, mocker):
     response = client.post(
         '/lament',
         data={'randos': 25, 'desired_level': 1},
-        follow_redirects=True,
-        base_url=LAMENT_SUBDOMAIN)
+        follow_redirects=True,)
     assert response.status_code == 200
     # Ensure we're sending a PDF
     assert response.mimetype == "application/pdf"
